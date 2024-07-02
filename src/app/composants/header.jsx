bc5ac2faaa0c0578846b1703f18914c80adeb65d
Cornu Noah <noah.cornu@icloud.com>
2024-07-02 11:05:19 +0200

    mise en place background

---
+++ src/app/composants/header.jsx
@@ -0,0 +1,15 @@
+import { headers } from "next/headers";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="/public/image/logo.png" alt="logo noah" />
+        <nav></nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
